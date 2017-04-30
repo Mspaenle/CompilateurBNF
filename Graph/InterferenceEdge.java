00c2a9ca3f600e4259aa6f77c3bec9a502c0636d
Mspaenle <Mahe.spaenle@etu.umontpellier.fr>
2017-04-30 15:04:33 +0200

    First Version

---
+++ Graph/InterferenceEdge.java
@@ -0,0 +1,14 @@
+package Graph;
+
+/**
+	*@author Assil El Yahyaoui - Mahé Spaenlé
+	*@version 1.0
+*/
+
+public class InterferenceEdge extends Edge{
+
+	public InterferenceEdge(Vertex v1, Vertex v2){
+		super(v1,v2);
+	}
+
+}
